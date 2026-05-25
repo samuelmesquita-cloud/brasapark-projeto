d1bd61361c4e3e4b9ca50fce69b46f17d1681713
sarahsoareslacerda <soares.lacerda@academico.ifpb.edu.br>
2026-05-25 10:33:09 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+// Função assíncrona responsável por carregar as atrações a partir de um arquivo JSON local.
+// O código faz uma requisição fetch para o arquivo "atracoes.json",
+// converte a resposta para JSON e retorna os dados para serem usados na aplicação.
+
 export async function carregarAtracoes() {
     const resposta = await fetch("../data/atracoes.json");
     return await resposta.json();
