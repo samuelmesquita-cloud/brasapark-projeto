d9128d39a29f80849d115f1714072477d003db8b
sarahsoareslacerda <soares.lacerda@academico.ifpb.edu.br>
2026-05-25 10:45:08 -0300

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,36 @@
 // O código faz uma requisição fetch para o arquivo "atracoes.json",
 // converte a resposta para JSON e retorna os dados para serem usados na aplicação.
 
-export async function carregarAtracoes() {
-    const resposta = await fetch("../data/atracoes.json");
-    return await resposta.json();
+const BASE_URL = "http://localhost:3000";
+
+export async function getAtracoes() {
+  const res = await fetch(`${BASE_URL}/atracoes`);
+  return res.json();
+}
+
+export async function createAtracao(data) {
+  return fetch(`${BASE_URL}/atracoes`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+}
+
+export async function deleteAtracao(id) {
+  return fetch(`${BASE_URL}/atracoes/${id}`, {
+    method: "DELETE"
+  });
+}
+
+export async function getClientes() {
+  const res = await fetch(`${BASE_URL}/clientes`);
+  return res.json();
+}
+
+export async function createCliente(data) {
+  return fetch(`${BASE_URL}/clientes`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
 }
