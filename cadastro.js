c65db238c5716d1cf6d0db46e085c3b4aba30086
sarahsoareslacerda <soares.lacerda@academico.ifpb.edu.br>
2026-05-25 10:37:29 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,10 @@
+// Função responsável por configurar o formulário de cadastro de clientes.
+// Captura os dados inseridos pelo usuário (nome, email e telefone),
+// cria um objeto de cliente com ID único e data de cadastro,
+// e armazena essas informações no localStorage do navegador.
+// Também mantém os dados já existentes, adiciona o novo cliente,
+// salva novamente e limpa o formulário após o envio.
+
 export function configurarCadastro() {
     const form = document.querySelector("form");
 
