d80579ea1b8fda34681cdad337720efbecf0440a
samuelmesquita-cloud <samuel.mesquita@academico.ifpb.edu.br>
2026-06-15 09:09:35 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,7 @@
-// Configura e inicia o servidor da aplicação utilizando Node.js e Express.
-// O código prepara a aplicação para receber requisições, organizar as rotas
-// das atrações, permitir comunicação com outras aplicações e registrar logs
-// das requisições no terminal. O servidor é iniciado na porta 3000.
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 
 const atracoesRoutes = require("./routes/atracoes");
 const clientesRoutes = require("./routes/clientes");
@@ -16,6 +12,9 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "frontend")));
 
 app.use("/atracoes", atracoesRoutes);
 app.use("/clientes", clientesRoutes);
