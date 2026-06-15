9d8064a6b1c5d65f535a034a02786ddf0ff7dcf7
Eduardo Ximenes <eduardo.ximenes@academico.ifpb.edu.br>
2026-06-15 18:05:53 -0300

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import morgan from "morgan";
+
+import atracaoRoutes from "./routes/atracaoRoutes";
+import clienteRoutes from "./routes/clienteRoutes";
+import { errorHandler } from "./middleware/errorHandler";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/atracoes", atracaoRoutes);
+app.use("/clientes", clienteRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server rodando na porta ${PORT}`);
+});
