1198e5410b3cb6039e9d44de1c6bb0bd172448d3
Eduardo Ximenes <eduardo.ximenes@academico.ifpb.edu.br>
2026-06-15 18:31:15 -0300

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,37 +1,27 @@
-// Função assíncrona responsável por carregar as atrações a partir de um arquivo JSON local.
-// O código faz uma requisição fetch para o arquivo "atracoes.json",
-// converte a resposta para JSON e retorna os dados para serem usados na aplicação.
+const API = "http://localhost:3000";
 
-const BASE_URL = "http://localhost:3000";
+async function request(url, options) {
+  const res = await fetch(API + url, options);
 
-export async function getAtracoes() {
-  const res = await fetch(`${BASE_URL}/atracoes`);
+  if (!res.ok) throw new Error("Erro API");
+
   return res.json();
 }
 
-export async function createAtracao(data) {
-  return fetch(`${BASE_URL}/atracoes`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
-}
+export const api = {
+  getAtracoes: () => request("/atracoes"),
 
-export async function deleteAtracao(id) {
-  return fetch(`${BASE_URL}/atracoes/${id}`, {
-    method: "DELETE"
-  });
-}
+  createAtracao: (data) =>
+    request("/atracoes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }),
 
-export async function getClientes() {
-  const res = await fetch(`${BASE_URL}/clientes`);
-  return res.json();
-}
-
-export async function createCliente(data) {
-  return fetch(`${BASE_URL}/clientes`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
-}
+  createCliente: (data) =>
+    request("/clientes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    })
+};
